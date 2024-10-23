6705fc02f0870d50900ea07f742038e5011e45c5
Balogh Dániel <100405854+baloghdaniel654@users.noreply.github.com>
2024-10-23 23:55:48 +0200

    Grid.cs class

--- IKT_Projekt_1/Szimulacio_Lib/Grid.cs
+++ IKT_Projekt_1/Szimulacio_Lib/Grid.cs
@@ -1,1 +1,21 @@
-﻿
+﻿namespace Szimulacio_Lib
+{
+    public class Grid
+    {
+        public int[,] Grass { get; private set; }
+        public int[,] Rabbits { get; private set; }
+        public int[,] Foxes { get; private set; }
+        public const int GridSize = 20;
+        private Random random;
+
+        public Grid()
+        {
+            Grass = new int[GridSize, GridSize];
+            Rabbits = new int[GridSize, GridSize];
+            Foxes = new int[GridSize, GridSize];
+            random = new Random();
+        }
+
+
+    }
+}
