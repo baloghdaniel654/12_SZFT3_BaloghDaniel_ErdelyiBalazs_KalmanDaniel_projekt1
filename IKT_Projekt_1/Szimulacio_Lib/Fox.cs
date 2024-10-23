87853f9ab9eb44bb0a2ddda0d1a4621c5e168601
Balogh Dániel <100405854+baloghdaniel654@users.noreply.github.com>
2024-10-23 23:59:35 +0200

    Fox.cs

--- IKT_Projekt_1/Szimulacio_Lib/Fox.cs
+++ IKT_Projekt_1/Szimulacio_Lib/Fox.cs
@@ -1,1 +1,19 @@
-﻿
+﻿namespace Szimulacio_Lib
+{
+    public class Fox
+    {
+        private const int maxFoxSatiety = 30;
+        private Grid grid;
+
+        public Fox(Grid grid)
+        {
+            this.grid = grid;
+        }
+
+        public void Place(int x, int y)
+        {
+            grid.Foxes[x, y] = new Random().Next(maxFoxSatiety / 2, maxFoxSatiety + 1);
+        }
+
+    }
+}
