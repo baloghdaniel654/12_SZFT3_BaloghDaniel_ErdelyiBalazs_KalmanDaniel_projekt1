203e700447271a814578f331b3c8021eb98a4609
ErdelyiBalazs06 <100405852+ErdelyiBalazs06@users.noreply.github.com>
2024-10-24 11:51:10 +0200

    Reproduce

--- IKT_Projekt_1/Szimulacio_Lib/Rabbit.cs
+++ IKT_Projekt_1/Szimulacio_Lib/Rabbit.cs
@@ -9,5 +9,21 @@ namespace Szimulacio_Lib
         {
             this.grid = grid;
         }
+
+          public void Update()
+  {
+      for (int i = 0; i < Grid.GridSize; i++)
+      {
+          for (int j = 0; j < Grid.GridSize; j++)
+          {
+              if (grid.Rabbits[i, j] > 0)
+              {
+                  EatGrass(i, j);
+                  Move(i, j);
+              }
+          }
+      }
+      Reproduce();
+  }
     }
 }
