f8ea4922a6657426b35d95e19be8ec41d91b0d85
ErdelyiBalazs06 <100405852+ErdelyiBalazs06@users.noreply.github.com>
2024-10-24 11:47:13 +0200

    Alap dolgok

--- IKT_Projekt_1/Szimulacio_Lib/Rabbit.cs
+++ IKT_Projekt_1/Szimulacio_Lib/Rabbit.cs
@@ -1,7 +1,13 @@
-﻿namespace Szimulacio_Lib
+namespace Szimulacio_Lib
 {
     public class Rabbit
     {
-       
+        private const int maxRabbitSatiety = 5;
+        private Grid grid;
+
+        public Rabbit(Grid grid)
+        {
+            this.grid = grid;
+        }
     }
 }
